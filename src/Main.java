632f5c1124875193e39ec6788a09399b2e49fd68
ValiPetier22 <82037085+ValiPetier22@users.noreply.github.com>
2021-04-21 09:54:59 +0300

    Small modify to main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant();
 
-        restaurant.request(TipMeniu.POST, 2);
+        restaurant.request(TipMeniu.POST, 10);
 
     }
 }
